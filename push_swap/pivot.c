eb1fe2d1a3dbf35630c0063ed3c9359e55d27bc1
성욱최 <lizardsw@seong-ugchoeui-MacBookAir.local>
2022-05-18 23:41:56 +0900

    push_swap

--- push_swap/pivot.c
+++ push_swap/pivot.c
@@ -2,35 +2,34 @@
 
 void AtoB(t_pocket *pocket, int start, int end)
 {
-	int pivot1;
-	int pivot2;
+	int pivot[2];
 	int range;
 	int i;
 	int j;
 	
 	i = 0;
 	j = 0;
-	range = setting_pivot(start, end, &pivot1, &pivot2);
-	ft_printf("\n****************AtoB start!****************");
-	ft_printf("\nstart : %d, end : %d pivot1 : %d, pivot2 : %d\n",start, end, pivot1, pivot2);
-	ft_printf("(%d ~ %d) (%d ~ %d) (%d ~ %d)\n\n", start, pivot1, pivot1 + 1, pivot2, pivot2 + 1, end);
+	range = setting_pivot(start, end, &pivot[0], &pivot[1]);
+	//ft_printf("\n****************AtoB start!****************");
+	//ft_printf("\nstart : %d, end : %d pivot1 : %d, pivot2 : %d\n",start, end, pivot1, pivot2);
+	//ft_printf("(%d ~ %d) (%d ~ %d) (%d ~ %d)\n\n", start, pivot1, pivot1 + 1, pivot2, pivot2 + 1, end);
 	if (range <= 2)
 	{
 		AtoB_small_sort(pocket, start, end);
-		show_deq(pocket -> A);
-		show_deq(pocket -> B);
+	//	show_deq(pocket -> A);
+	//	show_deq(pocket -> B);
 		return;
 	}
 	while (range >= 1)
 	{
-		if (pocket -> A -> start -> index > pivot2)
+		if (pocket -> A -> start -> index > pivot[1])
 		{
 			ft_rotate(pocket , 1);
 			j++;
 		}
 		else
 		{
-			if (pocket -> A -> start -> index <= pivot1)
+			if (pocket -> A -> start -> index <= pivot[0])
 				ft_push(pocket, 2);
 			else
 			{
@@ -40,9 +39,9 @@ void AtoB(t_pocket *pocket, int start, int end)
 			}
 		}
 		range--;
-		show_deq(pocket -> A);
-		show_deq(pocket -> B);
-		ft_printf("\n");
+	//	show_deq(pocket -> A);
+	//	show_deq(pocket -> B);
+	//	ft_printf("\n");
 	}
 	while (i != 0)
 	{
@@ -54,46 +53,46 @@ void AtoB(t_pocket *pocket, int start, int end)
 		ft_revrotate(pocket, 1);
 		j--;
 	}
-	ft_printf("\nreroate finished!\n\n");
-	show_deq(pocket -> A);
-	show_deq(pocket -> B);
-	ft_printf("\n");
-	AtoB(pocket, pivot2 + 1, end);
-	BtoA(pocket, pivot1 + 1, pivot2);
-	BtoA(pocket, start, pivot1);
+	//ft_printf("\nreroate finished!\n\n");
+	//show_deq(pocket -> A);
+	//show_deq(pocket -> B);
+	//ft_printf("\n");
+	AtoB(pocket, pivot[1] + 1, end);
+	BtoA(pocket, pivot[0] + 1, pivot[1]);
+	BtoA(pocket, start, pivot[0]);
 }
 
 void BtoA(t_pocket *pocket, int start, int end)
 {
-	int pivot1;
-	int pivot2;
+	int pivot[2];
 	int range;
 	int i;
 	int j;
+	int k;
 	
 	i = 0;
 	j = 0;
-	range = setting_pivot(start, end, &pivot1, &pivot2);
-	ft_printf("\n******************BtoA start!********************");
-	ft_printf("\nstart : %d, end : %d pivot1 : %d, pivot2 : %d\n",start, end, pivot1, pivot2);
-	ft_printf("(%d ~ %d) (%d ~ %d) (%d ~ %d)\n\n", start, pivot1, pivot1 + 1, pivot2, pivot2 + 1, end);
+	range = setting_pivot(start, end, &pivot[0], &pivot[1]);
+	//ft_printf("\n******************BtoA start!********************");
+	//ft_printf("\nstart : %d, end : %d pivot1 : %d, pivot2 : %d\n",start, end, pivot1, pivot2);
+	//ft_printf("(%d ~ %d) (%d ~ %d) (%d ~ %d)\n\n", start, pivot1, pivot1 + 1, pivot2, pivot2 + 1, end);
 	if (range <= 2)
 	{
 		BtoA_small_sort(pocket, start, end);
-		show_deq(pocket -> A);
-		show_deq(pocket -> B);
+	//	show_deq(pocket -> A);
+	//	show_deq(pocket -> B);
 		return;
 	}
 	while (range >= 1)
 	{
-		if (pocket -> B -> start -> index <= pivot1)
+		if (pocket -> B -> start -> index <= pivot[0])
 		{
 			ft_rotate(pocket , 2);
 			j++;
 		}
 		else
 		{
-			if (pocket -> B -> start -> index > pivot2)
+			if (pocket -> B -> start -> index > pivot[1])
 				ft_push(pocket, 1);
 			else
 			{
@@ -101,28 +100,29 @@ void BtoA(t_pocket *pocket, int start, int end)
 				ft_rotate(pocket, 1);
 				i++;
 			}
+			k++;
 		}
-		show_deq(pocket -> A);
-		show_deq(pocket -> B);
+	//	show_deq(pocket -> A);
+	//	show_deq(pocket -> B);
 		range--;
 	}
-	ft_printf("\nreroate finished!\n\n");
-	show_deq(pocket -> A);
-	show_deq(pocket -> B);
+	//ft_printf("\nreroate finished!\n\n");
+	//show_deq(pocket -> A);
+	//show_deq(pocket -> B);
 	
-	AtoB(pocket, pivot2 + 1, end);
+	AtoB(pocket, pivot[1] + 1, end);
 	while (i != 0)
 	{
 		ft_revrotate(pocket, 1);
 		i--;
 	}
-	AtoB(pocket, pivot1 + 1, pivot2);
+	AtoB(pocket, pivot[0] + 1, pivot[1]);
 	while (j != 0)
 	{
 		ft_revrotate(pocket, 2);
 		j--;
 	}
-	BtoA(pocket, start, pivot1);
+	BtoA(pocket, start, pivot[0]);
 }
 
 int setting_pivot(int start, int end, int *pivot1, int *pivot2)
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -12,21 +12,21 @@ int main(int argc, char **argv)
 	B = new_deq();
 	rule = new_deq();
 	pocket = new_pocket(A, B, rule);
-
 	str = total_str(argv, argc);
-	ft_printf("%s\n", str);
+	//ft_printf("%s\n", str);
 	if (get_number(argv, argc, A) == -1)
 		return 0;
-	show_deq(A);
+	//show_deq(A);
 	sort_index_deq(A);
-	show_deq(A);
-	show_deq(B);
+	//show_deq(A);
+	//show_deq(B);
 	
-	ft_printf("--------------------\n");
+	//ft_printf("--------------------\n");
 	AtoB(pocket, 1, A -> size);
-	show_deq(A);
-	show_deq(B);
-	show_deq(rule);
+	//show_deq(A);
+	//show_deq(B);
+	//show_deq(rule);
+
 	rule_show_deq(rule);
 
 
